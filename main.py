f334bf24a3bb7980fe752a15808de139726fdf15
Atef <atefsarsour2011@gmail.com>
2022-10-07 04:09:56 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+value1= input("Enter Value1:")
+value2= input("Enter Value2::")
+value3= input("Enter Value3:")
+value4= input("Enter Value4:")
+value5= input("Enter Value5:")
+array = [value1,value2,value3,value4,value5]
+
+print (max(array))
+print(min(array))
+#Thanks for your efforts Eng.Bahha
